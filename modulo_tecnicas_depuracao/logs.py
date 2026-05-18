b3a5f3680c6dc1fd693be10bcfe18ec53021a9a4
dhouglas <dhouglasbn@gmail.com>
2026-05-18 19:02:46 -0300

    class: finished module debug

---
+++ modulo_tecnicas_depuracao/logs.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logging.info('Programa iniciado')
+logging.debug('Mensagem de debug')
+logging.warning('Mensagem de warning')
+logging.error('Mensagem de erro')
+logging.critical('Mensagem de critical')
---
+++ modulo_tecnicas_depuracao/raiseIdade.py
@@ -0,0 +1,4 @@
+idade = -5
+
+if idade < 0:
+  raise ValueError('Idade não pode ser negativa')
